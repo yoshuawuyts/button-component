5edccea118373c28b5f9c8e43ed0ae4749fcd2ce
Yoshua Wuyts <i@yoshuawuyts.com>
2015-04-06 01:21:17 -0700

    example: fix bug in inheritence

--- example/button.js
+++ example/button.js
@@ -1,21 +1,22 @@
-const BtnProto = require('../')
 const symbol = require('es6-symbol')
+const BtnProto = require('../')
 
+const btn = Object.create(BtnProto)
 const count = symbol('count')
 
-module.exports = BtnProto
+module.exports = btn
 
-BtnProto.on('created', function () {
+btn.on('created', function () {
   this[count] = 0
   this.textContent = textContent(this[count])
 })
 
-BtnProto.on('attribute', function (name, old, nw) {
+btn.on('attribute', function (name, old, nw) {
   if (name !== 'count') return
   this.count = nw
 })
 
-Object.defineProperty(BtnProto.prototype, 'count', {
+Object.defineProperty(btn.prototype, 'count', {
   get: function () {return this[count]},
   set: function (value) {
     if (this[count] === value) return
