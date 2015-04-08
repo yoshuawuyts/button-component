d5fb6c5443b339b690486bf274e20c2d5f874550
Yoshua Wuyts <i@yoshuawuyts.com>
2015-04-07 22:57:39 -0700

    example: use ES6 string interpolation syntax

--- example/button.js
+++ example/button.js
@@ -2,13 +2,18 @@ const symbol = require('es6-symbol')
 const BtnProto = require('../')
 
 const btn = Object.create(BtnProto)
+const textContent = symbol('textContent')
 const count = symbol('count')
 
 module.exports = btn
 
 btn.on('created', function () {
   this[count] = 0
-  this.textContent = textContent(this[count])
+})
+
+btn.on('attached', function () {
+  this[textContent] = this.textContent
+  this.textContent = fmtText(this[textContent], this[count])
 })
 
 btn.on('attribute', function (name, old, nw) {
@@ -21,12 +26,12 @@ Object.defineProperty(btn.prototype, 'count', {
   set: function (value) {
     if (this[count] === value) return
     this[count] = value
-    this.textContent = textContent(this[count])
+    this.textContent = fmtText(this[textContent], this[count])
   }
 })
 
 // create the text content
-// textContent(Number:count) -> String:content
-function textContent (count) {
-  return 'clicked ' + count + ' times'
+// textContent(String:content, Number:count) -> String:content
+function fmtText (content, count) {
+  return content.replace(/\$\{count\}/g, count)
 }
--- example/index.js
+++ example/index.js
@@ -1,10 +1,12 @@
 // create instance
 document.registerElement('custom-button', require('./button'))
 const customButton = document.createElement('button', 'custom-button')
-
-document.body.appendChild(customButton)
+customButton.innerText = 'clicked ${count} times'
 
 // update the click counter and set the clicked text
 customButton.addEventListener('click', function () {
   this.count = this.count + 1
 })
+
+// attach button to dom
+document.body.appendChild(customButton)
