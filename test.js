e8370a143619fd503558f168fa5a76634caecb6f
Yoshua Wuyts <i@yoshuawuyts.com>
2015-04-10 03:32:46 -0700

    test: init

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
-// const test = require('tape')
+require('webcomponents.js')
 
-// test('', function (t) {
-//   t.plan(0)
-// })
+const test = require('tape')
+
+document.registerElement('custom-button', require('./'))
+
+test('should set prototype correctly', function (t) {
+  t.plan(3)
+  const el = document.createElement('button', 'custom-button')
+  t.equal(el.nodeName, 'BUTTON')
+  t.ok(el instanceof window.HTMLButtonElement, 'prototype')
+  t.equal(el.getAttribute('is'), 'custom-button')
+})
